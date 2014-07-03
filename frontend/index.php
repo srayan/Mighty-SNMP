191acd5724f2bc07ead9f981c08d977cca9a9f17
Srayan Guhathakurta <srayanguhathakurta@yahoo.com>
2014-07-03 16:56:43 -0500

    Create index.php

---
+++ frontend/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Book Worm</title>
+
+  <script src="_scripts/jquery.js"></script>
+
+  <link rel="stylesheet" href="_css/foundation.css">
+  <link rel="stylesheet" href="_css/main.css">
+
+  <link rel="Shortcut icon" href="_images/snmp.gif">
+
+  <!--<script src="_scripts/modernizr.foundation.js"></script>-->
+  <style>
+  h2 {
+	  font-size: 160%;
+  }
+  </style>
+</head>
+<body>
+
+  <!-- Main Content -->
+  <div class="row">
+    <?php include 'sidebar.php'; ?>
+
+    <article class="main nine columns">
+       
+        <div align="center"> <h1> Mighty SNMP </h1> </div>
+      
+
+        <p>Mighty SNMP helps is an interactive web service to let you manage your Home Network remotely.
+          <ul class="textIndent">
+            <li>Current features let you monitor your</li>
+            <li>UPS</li>
+            <li>Home Thermostat</li>
+            <li>Intelligent refrigerator</li>
+          </ul>
+        </p>
+
+        <p><i>Go ahead and enjoy the remote edge </i></p>
+    </article>
+
+  </div>
+  
+
+<script src="_scripts/foundation.min.js"></script>
+  <script src="_scripts/app.js"></script>
+</body>
+</html>
