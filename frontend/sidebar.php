b46353fd3f8bfb69fca41ef2256d22904a5e4ec5
Srayan Guhathakurta <srayanguhathakurta@yahoo.com>
2014-07-03 17:34:35 -0500

    Create sidebar.php

---
+++ frontend/sidebar.php
@@ -0,0 +1,23 @@
+<aside class="related three columns" id="actionSidebar">
+      <a href="index.php"><img src="_images/home.gif"></a>
+
+      <br>
+
+      <div id="buttonHolder">
+        <ul type="none">
+          
+          <a href="search.php">
+            <li><img src="_images/search.png"> &nbsp; UPS</li>
+          </a>
+          <a href="checkout.php">
+            <li><img src="_images/checkout.png"> &nbsp; Intelligent Refrigerator</li>
+          </a>
+          <a href="checkin.php">
+            <li><img src="_images/checkin.png"> &nbsp; Thermostat</li>
+          </a>
+          <a href="docs.php">
+            <li><img src="_images/info.png"> &nbsp; About Me</li>
+          </a>
+        </ul> 
+      </div>
+</aside>
